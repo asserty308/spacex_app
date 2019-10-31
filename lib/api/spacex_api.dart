031eaa2b937bf55deb407e99dac864e818e6ad97
asserty308 <yannickassert@gmail.com>
2019-10-31 19:52:57 +0100

    launches api

--- lib/api/spacex_api.dart
+++ lib/api/spacex_api.dart
@@ -10,9 +10,30 @@ class SpaceXAPI {
     return json.decode(response.body);
   }
 
+  // Launches
+
+  Future<dynamic> getLatestLaunch() async {
+    final url = '${_baseUrl}launches/latest/';
+    return await getJSON(url);
+  }
+
   Future<dynamic> getNextLaunch() async {
     final url = '${_baseUrl}launches/next/';
-    final response = await getJSON(url);
-    return response;
-  } 
+    return await getJSON(url);
+  }
+
+  Future<dynamic> getAllLaunches() async {
+    final url = '${_baseUrl}launches/';
+    return await getJSON(url);
+  }
+
+  Future<dynamic> getPastLaunches() async {
+    final url = '${_baseUrl}launches/past';
+    return await getJSON(url);
+  }
+
+  Future<dynamic> getUpcomingLaunches() async {
+    final url = '${_baseUrl}launches/upcoming/';
+    return await getJSON(url);
+  }
 }
