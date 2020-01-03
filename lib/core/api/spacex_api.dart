5f19844fbc0396493caf6af6c1065ec851501989
asserty308 <yannickassert@gmail.com>
2020-01-03 19:27:58 +0100

    refactor

--- lib/core/api/spacex_api.dart
+++ lib/core/api/spacex_api.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_core/web/base_api.dart';
 
 class SpaceXApi {
-  final _api = BaseAPI('https://api.spacexdata.com/v3/');
+  final _api = BaseApi('https://api.spacexdata.com/v3/');
 
   /// Wrapper to fetch JSON from the API
   Future<dynamic> fetchJSON(String path) async {
