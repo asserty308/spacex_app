dce5a3c21b41a043786c1bd66e3d8e43b85dd85d
asserty308 <yannickassert@gmail.com>
2020-08-02 00:49:50 +0200

    refactoring

--- lib/features/rockets/ui/screens/rocket_details.dart
+++ lib/features/rockets/ui/screens/rocket_details.dart
@@ -5,7 +5,7 @@ import 'package:spacex_guide/features/rockets/ui/screens/all_rockets_screen.dart
 import 'package:spacex_guide/features/rockets/ui/widgets/rocket_carousel.dart';
 import 'package:spacex_guide/features/rockets/ui/widgets/rocket_info.dart';
 
-class RocketDetailsScreen extends StatefulWidget {
+class RocketDetailsScreen extends StatelessWidget {
   const RocketDetailsScreen({
     this.rocket,
     this.allRocketsScreen,
@@ -13,40 +13,41 @@ class RocketDetailsScreen extends StatefulWidget {
 
   final Rocket rocket;
   final AllRocketsScreen allRocketsScreen;
-  
-  @override
-  _RocketDetailsScreenState createState() => _RocketDetailsScreenState();
-}
 
-class _RocketDetailsScreenState extends State<RocketDetailsScreen> {
   @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        shouldReloadRocketCarousel = true;
-        return true;
-      },
-      child: Scaffold(
-        body: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: ImageCarousel(
-                    imageUrls: widget.rocket.flickrImages,
-                  ),
-                ),
-              )
-            ];
-          },
-          body: Container(
-            child: RocketInfo(rocket: widget.rocket,),
-          )
-        ),
+  Widget build(BuildContext context) => WillPopScope(
+    onWillPop: () async {
+      shouldReloadRocketCarousel = true;
+      return true;
+    },
+    child: _scaffold,
+  );
+
+  // Widgets
+
+  Widget get _scaffold => Scaffold(
+    body: _scrollView,
+  );
+
+  Widget get _scrollView => NestedScrollView(
+    headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
+      _sliverAppBar,
+    ],
+    body: _body,
+  );
+
+  Widget get _sliverAppBar => SliverAppBar(
+    expandedHeight: 200.0,
+    floating: false,
+    pinned: true,
+    flexibleSpace: FlexibleSpaceBar(
+      background: ImageCarousel(
+        imageUrls: rocket.flickrImages,
       ),
-    );
-  }
+    ),
+  );
+
+  Widget get _body => Container(
+    child: RocketInfo(rocket: rocket,),
+  );
 }
