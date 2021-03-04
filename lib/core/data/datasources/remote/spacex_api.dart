a180217860ad208ff073da7ef1c41c08bb861ea0
asserty308 <yannickassert@gmail.com>
2021-03-04 21:29:08 +0100

    update

--- lib/core/data/datasources/remote/spacex_api.dart
+++ lib/core/data/datasources/remote/spacex_api.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_web/datasources/base_api.dart';
+import 'package:flutter_web/services/base_api.dart';
 
 abstract class SpaceXApi extends BaseApi {
   SpaceXApi() : super('https://api.spacexdata.com/v4/');
